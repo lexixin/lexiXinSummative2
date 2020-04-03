6ec2e755c0016978aed127702e8dc63709ef6db3
lexixin <lexi19961205@gmail.com>
2020-04-03 13:05:03 +1300

    update

--- summativeTwo/js/main.js
+++ summativeTwo/js/main.js
@@ -1,16 +1,11 @@
 console.log("HELLO WORLD!");
 
 
+var navButton = document.getElementById('navButton');
+var menu = document.getElementById("menu");
+navButton.addEventListener('click', toggleMenu);
 function toggleMenu() {
-
-    //SET X TO BE OUR #menu ELEMENT
-    var x = document.getElementById("menu");
-    //SET Y TO BE OUR #navButton ELEMENT
-    var y = document.getElementById("navButton");
-
-    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
-    x.classList.toggle("displayMenu");
-    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
-    y.classList.toggle("fas fa-times")
-
+    menu.classList.toggle("displayMenu");
+    navButton.classList.toggle("fa-times");
 }
+
